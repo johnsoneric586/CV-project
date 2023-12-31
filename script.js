711781b62321608024532d32de61bf47d2b70856
Eric M. Johnson <johnsoneric586@gmail.com>
2023-12-31 12:36:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+
+    hamburger.classList.toggle("active");
+  });
+});
